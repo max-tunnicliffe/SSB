872f69c0fe2958ddea9a85e6077b02463c02faf4
j tunnicliffe <jtunnicl@gmail.com>
2019-08-20 17:18:20 +1200

    Added leap year function

--- birthsecondsHARD.py
+++ birthsecondsHARD.py
@@ -1,6 +1,17 @@
 import datetime
 now = datetime.datetime.now()
 
+def leap_year(y):
+    if y % 400 == 0:
+        return True
+    if y % 100 == 0:
+        return False
+    if y % 4 == 0:
+        return True
+    else:
+        return False
+
+
 thingy = raw_input("when where you born?")
 
 year2 = int(thingy.split('/')[2])
